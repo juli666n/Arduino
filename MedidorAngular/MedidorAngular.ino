ea1c471cf4eb86d8510b7b23cf2449cd74d9feea
julianhfan <jchuertasf@yahoo.com>
2019-05-15 17:57:40 -0500

    cambios

--- MedidorAngular/MedidorAngular.ino
+++ MedidorAngular/MedidorAngular.ino
@@ -1,8 +1,9 @@
 #include <LiquidCrystal.h>
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
-int valor;
-int angulo;
+float valor = 0;
+float angulo = 0;
+int angulogrados = 0;
 
 void setup() {
   lcd.begin(16, 2);
@@ -10,10 +11,11 @@ void setup() {
 }
 void loop() {
   valor = analogRead(A0);
-  angulo = -946*valor+263.48;
+  angulo = valor*100/663;
+  angulogrados = map(angulo, 0, 100, 0, 275);
   lcd.print("angulo:");
   lcd.setCursor(1, 1);
-  lcd.print(angulo);
+  lcd.print(angulogrados);
   Serial.print(valor);
   Serial.print(";");
   delay(1000);
